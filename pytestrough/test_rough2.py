28c3dd915607d6eaf646e7cc561ea889c0b437d7
hrishi1315 <frycry1212@gmail.com>
2024-10-29 23:41:21 +0530

    changes done in rough2 and added new funct

--- pytestrough/test_rough2.py
+++ pytestrough/test_rough2.py
@@ -1,8 +1,8 @@
 import pytest
 
-def total_divisible_by_6(input_total):
+def total_divisible_by_22(input_total):
     assert input_total % 6 == 0
 
-def total_divisible_by_15(input_total):
+def total_divisible_by_33(input_total):
     assert input_total % 15 == 0
 
